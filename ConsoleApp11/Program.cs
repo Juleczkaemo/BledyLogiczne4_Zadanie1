4b51de45318447a479fed85bb108f152550e91ad
4Mgr1 <4Mgr1@DESKTOP-EHD8S3P>
2025-11-27 08:54:22 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 15, b = 8, c = 12;
+        int min = a;
+
+        if (b < min)
+        {
+            min = b;
+        }
+
+        if (c > min)
+        {
+            min = c;
+        }
+
+        Console.WriteLine($"Minimum: {min}");
+    }
+}
